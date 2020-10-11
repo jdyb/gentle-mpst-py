1fd14e00834cf31227ddc2c9da8642ba082b45af
Jon Dybeck <jon@dybeck.eu>
2020-10-11 16:22:25 +0200

    WIP: Project and merge

--- msgt.py
+++ msgt.py
@@ -7,7 +7,11 @@ Notes to reader:
       for simpler reading.
 """
 
-from typing import Dict, Tuple, Set
+from typing import Dict, Tuple, Set, List, FrozenSet
+
+class ExampleError(Exception):
+    """This is raised if an example does not produce the expected results"""
+    pass
 
 # Section 3 "Synchronous Multiparty Session Calculus", Notation 01 (Base Sets)
 
@@ -16,6 +20,8 @@ class Label(object):
         self.lname = name
     def __str__(self):
         return str(self.lname)
+    def __repr__(self):
+        return f'Label({self.lname})'
 
 class Participant(object):
     """Session participant, from "Notation 01 (Base sets)" """
@@ -23,39 +29,53 @@ class Participant(object):
         self.rname = name
     def __str__(self):
         return self.rname
-
-# FIXME From where are the sorts
+    def __repr__(self):
+        return f'Participant({self.rname})'
 
 class Sort(object):
-    """Sorts and global types"""
+    """Sorts"""
+    # FIXME From where are the sorts
     pass
 
-class Nat(Sort):
-    pass
+class SNat(Sort):
+    # FIXME Where is this defined
+    def __str__(self):
+        return 'nat'
+    def __repr__(self):
+        return 'SNat()'
 
-class Int(Sort):
-    pass
+class SInt(Sort):
+    # FIXME Where is this defined
+    def __repr__(self):
+        return 'SInt()'
 
-class Bool(Sort):
-    pass
+class SBool(Sort):
+    # FIXME Where is this defined
+    def __repr__(self):
+        return 'SBool()'
 
 # The subclasses of Local are from section "4.1 Types and Projectsions" and
 # "Definition 3 (Local Session Types)"
 
 class LocalT(object):
     """Local type."""
+
     def pt(self) -> Set[Participant]:
         raise NotImplementedError()
 
 class LEnd(LocalT):
     """Local termination"""
+
     def pt(self) -> Set[Participant]:
         return set()
 
 class LExternalChoice(LocalT):
-    """Local Type for External Choice"""
+    """External Choice / Branching Type"""
+    """The ampersand-thing and exclamation-mark-thing"""
+
     def __init__(self, p, alternatives: Dict[Label, Tuple[Sort,LocalT]]):
         self.p, self.alternatives = p, alternatives
+
     def pt(self) -> Set[Participant]:
         pts = set((self.p,))
         for label in self.alternatives:
@@ -63,10 +83,16 @@ class LExternalChoice(LocalT):
             pts.add(ltype.pt())
         return pts
 
+    def __repr__(self):
+        return f'LExternalChoice({repr(self.p)}, {repr(self.alternatives)})'
+
 class LInternalChoice(LocalT):
-    """Local Type for Internal Choice"""
+    """Internal Choice / Selection type"""
+    """circle-cross and questionmarks"""
+
     def __init__(self, q, alternatives: Dict[Label, Tuple[Sort,LocalT]]):
         self.q, self.alternatives = q, alternatives
+
     def pt(self) -> Set[Participant]:
         pts = set((self.q,))
         for label in self.alternatives:
@@ -74,68 +100,229 @@ class LInternalChoice(LocalT):
             pts.add(ltype.pt())
         return pts
 
+    def __repr__(self):
+        return f'LInternalChoice({repr(self.q)}, {repr(self.alternatives)})'
+
 class LVariable(LocalT):
+
     def __init__(self, name: str):
         self.ltvname = name
+
     def pt(self) -> Set[Participant]:
         return set()
 
+    def __repr__(self):
+        return f'LVariable({self.ltvname})'
+
 class LRec(LocalT):
+
     def __init__(self, ltvariable: LVariable, local_type: LocalT):
         self.ltvariable, self.local_type = ltvariable, local_type
+
     def pt(self) -> Set[Participant]:
         return self.local_type.pt()
 
+    def __repr__(self):
+        return f'LRec({self.ltvariable}, {repr(self.local_type)})'
 
 # The subclasses of GlobalT are from section "4.1 Types and Projections"
 # definition 2. 
 
 class GlobalT(object):
     """Global type"""
+
     def pt(self):
         """Compute the set of participants of a global type.
         Returns the set of participants."""
         raise NotImplementedError()
-    def simp_projection(self, r: Participant) -> LocalT:
-        """Simple (no merging) projection. See 'Definition 4'."""
+
+    def project(self, r: Participant) -> LocalT:
+        """Merging projection. See 'Definition 5'."""
         raise NotImplementedError()
 
 class GEnd(GlobalT):
     """Global type signifying terminated protocol."""
+
     def pt(self):
         return set()
-    def simp_projection(self, r: Participant) -> LocalT:
-        pass
+
+    def project(self, r: Participant) -> LocalT:
+        """[PROJ-END]"""
+        return LEnd()
 
 class GTVar(GlobalT):
     """Global type variable"""
-    def __init__(self, name):
+
+    def __init__(self, name: str):
         self.gtvname = name
+
     def pt(self):
         return set()
 
+    def project(self, r):
+        """[PROJ-VAR]"""
+        return LVariable(self.gtvname)
+
 class GRec(GlobalT):
     """Recursive global type"""
+
     def __init__(self, gtvariable: GTVar, global_type: GlobalT):
         self.gtvariable, self.global_type = gtvariable, global_type
+
     def pt(self):
         return self.global_type.pt()
 
+    def project(self, r):
+        """[PROJ-REC-1/2] [PROJ-REC-2]"""
+        pts = self.global_type.pt()
+        if r in pts:
+            # [PROJ-REC-1]
+            return LRec(self.gtvariable.project(r),
+                    self.global_type.project(r))
+        elif r not in pts:
+            # [PROJ-REC-2]
+            return LEnd()
+        else:
+            # Should never be reached.
+            raise RuntimeError()
+
 class GCom(GlobalT):
     """Global type for message communication between two participants."""
+
     def __init__(self, source, destination, alternatives):
         """Alternatives is a dict, label keys."""
-        self.source, self.destination, self.alternatives = \
-                source, destination, alternatives
-    def pt(self):
+        self.source: Participant = source
+        self.destination: Participant = destination
+        self.alternatives: Dict[Label, Tuple[Sort, GlobalT]] = alternatives
+
+    def pt(self) -> Set[Participant]:
         pts = set((self.source, self.destination))
         for label in self.alternatives:
             Si, Gi = self.alternatives[label]
             pts.add(Gi.pt())
         return pts
 
+    def _proj_in(self, r: Participant) -> LExternalChoice:
+        """ [PROJ-IN] """
+        local_alternatives = {}
+        for label in self.alternatives:
+            sort, global_type = self.alternatives[label]
+            local_alternatives[label] = (sort, global_type.project(r))
+        return LExternalChoice(self.source, local_alternatives)
+
+    def _proj_out(self, r: Participant) -> LExternalChoice:
+        """ [PROJ-OUT] """
+        local_alternatives = {}
+        for label in self.alternatives:
+            sort, global_type = self.alternatives[label]
+            local_alternatives[label] = (sort, global_type.project(r))
+        return LExternalChoice(self.destination, local_alternatives)
+
+    def _proj_cont(self, r: Participant):
+        """ [PROJ-CONT] """
+        # FIXME Return type?
+        # FIXME Check that alternatives has at least one element.
+        # Project each of the continuation global types
+        local_types: List[LocalT] = []
+        for label in self.alternatives:
+            sort, global_type = self.alternatives[label]
+            local_types.append(global_type.project(r))
+        # Now merge the local (session) types into a single local type. 
+        merged_type = local_types.pop()
+        for local_type in local_types:
+            tmp = merge(merged_type, local_type)
+            if tmp:
+                merged_type = tmp
+            else:
+                return None
+        return merged_type
+
+    def project(self, r):
+        """[PROJ-IN] [PROJ-OUT] [PROJ-CONT]"""
+        if self.destination == r:
+            return self._proj_in(r)
+        elif self.source == r:
+            return self._proj_out(r)
+        elif self.source != r and self.destination != r:
+            return self._proj_cont(r)
+        else:
+            # Should never be reached.
+            raise RuntimeError()
+
+def merge(T1: LocalT, T2: LocalT):
+    """Merging operator."""
+    """
+    While this could be added to each LocalT subclass, that mostly just
+    duplicates the [MRG-ID] case on everything but LExternalChoice, so we use
+    a function instead.
+    """
+    # FIXME Return type annotation. May return None. 
+    if T1 == T2:
+        # [MRG-ID]
+        return T1
+    elif isinstance(T1, LExternalChoice) and isinstance(T2, LExternalChoice):
+        # [MRG-BRA]
+        #
+        # FIXME Nicer way to write the condition?
+        # Note that the definition of the merging operator is a little bit
+        # unclear as to how to treat overlapping labels. The definition talks
+        # about k being a member of the union of J and I, but it does not make
+        # it clear how Tk is to be selected. Example 4 clarifies that when
+        # I and J has overlapping labels then the continuation types Ti and Tj
+        # must be the same. 
+        #
+        # Compute the label-sets, we will use these for iteration later.
+        labels_t1: FrozenSet[Label] = frozenset(T1.alternatives.keys())
+        labels_t2: FrozenSet[Label] = frozenset(T2.alternatives.keys())
+        shared_labels = labels_t1.intersection(labels_t2)
+        labels_only_t1 = labels_t1 - shared_labels
+        labels_only_t2 = labels_t2 - shared_labels
+        # Check that T1 and T2 have the same participant (p')
+        if T1.p != T2.p:
+            # The merge operator is undefined for this case.
+            return None
+        # Make empty T3 type, we will fill it in the following blocks of code.
+        T3 = LExternalChoice(T1.p, {})
+        # Collect type continuations exclusive to T1 into T3
+        for label in labels_only_t1:
+            T3.alternatives[label] = T1.alternatives[label]
+        # Collect type continuations exclusive to T2 into T3
+        for label in labels_only_t2:
+            T3.alternatives[label] = T2.alternatives[label]
+        # Check that the type continuations with labels shared between T1 and
+        # T2 are the same. Then collect the type continuations into T3.
+        # If the type continuations are not the same abort the merge.
+        for label in shared_labels:
+            t1c = T1.alternatives[label]
+            t2c = T2.alternatives[label]
+            if t1c == t2c:
+                T3.alternatives[label] = t1c
+            else:
+                # The continuations do not match, so the merge is undefined.
+                return None
+        return T3
+    else:
+        return None
+
+def example_4():
+    # Participants and labels used in the example.
+    p, q = Participant('p'), Participant('q')
+    l = Label('l')
+    # First line in example.
+    tst1 = LInternalChoice(q,{l:(SNat(),LEnd())})
+    if merge(tst1, tst1) != tst1:
+        raise ExampleError((example_4, 1))
+    # Second line in example (undefined due to different participants).
+    if merge(LInternalChoice(p, {l: (SNat(), LEnd())}),
+            LInternalChoice(q,{l: (SNat(), LEnd())})) != None:
+        raise ExampleError((example_4, 2))
+
+def project(G: GlobalT, r: Participant) -> LocalT:
+    """Wrapper function for projection in definition 5"""
+    return G.project(r)
+
 class Expression(object):
-    pass
+    pass # Intentionally empty superclass.
 
 class Succ(Expression):
     def __init__(self, arg):
@@ -183,7 +370,7 @@ class Inaction(Process):
         return None
 
 class CannotCommunicate(Exception):
-    pass
+    pass # Intentionally empty exception.
 
 class Send(Process):
     def __init__(self, destination, label, expr, continuation):
@@ -262,15 +449,6 @@ class ExtChoice(Process):
         # No alternative can communicate.
         raise CannotCommunicate()
 
-Bob, Alice, Carol = Participant('Bob'), Participant('Alice'), Participant('Carol')
-l1, l2, l3, l4 = Label(1), Label(2), Label(3), Label(4)
-x = Variable('x')
-
-PAlice = Send(Bob, l1, 50, Recv(Carol, l3, x, Inaction()))
-PBob = ExtChoice(Recv(Alice, l1, x, Send(Carol, l2, 100, Inaction())),
-        Recv(Alice, l4, x, Send(Carol, l2, 2, Inaction())))
-PCarol = Recv(Bob, l2, x, Send(Alice, l3, Succ(x), Inaction()))
-
 class MState(object):
     def __init__(self, participants):
         Process.__init__(self)
@@ -297,9 +475,59 @@ class MState(object):
                 for role in self.participants]
         return 'MState(\n{}\t)'.format(''.join(strs))
 
-state = MState({Alice: PAlice, Bob: PBob, Carol: PCarol})
 
+# FIXME Wrap in function. Add reference to example.
+Bob, Alice, Carol = \
+        Participant('Bob'), Participant('Alice'), Participant('Carol')
+l1, l2, l3, l4 = Label(1), Label(2), Label(3), Label(4)
+x = Variable('x')
+PAlice = Send(Bob, l1, 50, Recv(Carol, l3, x, Inaction()))
+PBob = ExtChoice(Recv(Alice, l1, x, Send(Carol, l2, 100, Inaction())),
+        Recv(Alice, l4, x, Send(Carol, l2, 2, Inaction())))
+PCarol = Recv(Bob, l2, x, Send(Alice, l3, Succ(x), Inaction()))
+state = MState({Alice: PAlice, Bob: PBob, Carol: PCarol})
 while state:
     print(repr(state))
     state = state.step()
+
+def section_4_1_example_5():
+    """Section 4.1 "Types and Projections", Example 5"""
+    # Notes about this example
+    #
+    # Arrow in the publication becomes GCom class, int and nat are
+    # SInt and SNat to avoid collision with Python reserved names. The GEnd is
+    # omitted in the example, as stated at page 78
+    # (Section 3 "Synchronous Multiparty Session Calculus"): 
+    #   "We often omit 0 from the tail of processes"
+    # Additionally this example uses infix operators for both internal and
+    # external choice (the crossed circle and ampersands), these are
+    # implemented as LInternalChoice and LExternalChoice constructions.
+    #
+    # Define the participants used in this example.
+    q, p, r = Participant('q'), Participant('p'), Participant('r')
+    # Define the labels used in this example.
+    l1, l2, l3, l4, l5 = \
+            Label('l1'), Label('l2'), Label('l3'), Label('l4'), Label('l5')
+    # Define the global type
+    G1 = GCom(q, r, {l3: (SInt(), GEnd()), l5: (SNat(), GEnd())})
+    G2 = GCom(q, r, {l4: (SInt(), GEnd()), l5: (SNat(), GEnd())})
+    G = GCom(p, q, {l1: (SNat(), G1), l2: (SBool(), G2)})
+    # Local types of participants q, p and r
+    Lq, Lp, Lr = G.project(q), G.project(p), G.project(r)
+    # Expected local type of participant p
+    Lp_ = LInternalChoice(q, {l1: (SNat(), LEnd()), l2: (SBool(), LEnd())})
+    # Expected local type of participant q
+    Lq_0 = LInternalChoice(r, {l3: (SInt(), LEnd()), l5: (SNat(), LEnd())})
+    Lq_1 = LInternalChoice(r, {l5: (SInt(), LEnd()), l5: (SNat(), LEnd())})
+    Lq_ = LExternalChoice(p, {l1: (SNat(), Lq_0), l2: (SBool(), Lq_1)})
+    # Expected local type of participant r
+    Lr_ = LExternalChoice(q, {l3: (SInt(), LEnd()), l4: (SInt(), LEnd()),
+        l5: (SNat(), LEnd())})
+    # Check that projected local types match the expected local types, to make
+    # sure this example actually works.
+    if Lq != Lq_ or Lp != Lp_ or Lr != Lr_:
+        raise ExampleError((section_4_1_example_5, Lq, Lp, Lr))
+
+example_4()
+section_4_1_example_5()
 
